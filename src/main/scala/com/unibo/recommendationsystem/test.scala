aa7aa41451df86a2ea557533d2ca698c14986eb3
Leonardo Vincenzi <leovincenzi98@gmail.com>
2024-02-29 17:44:25 +0100

    Add test

--- src/main/scala/com/unibo/recommendationsystem/test.scala
+++ src/main/scala/com/unibo/recommendationsystem/test.scala
@@ -3,12 +3,15 @@ package com.unibo.recommendationsystem
 import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+
+import java.io.{File, FileNotFoundException, PrintWriter}
+
 object test {
   def main(args: Array[String]): Unit = {
     //recommend type, use --M for recommend users from a game or --U for recommend games from a user
     val recommendType = "--U"
-    val inputID = "51580" // input id or game id
-
+    val inputID = "607050" // input id or game id
+    //607050 user
     println("---------Initializing Spark-----------")
     val sparkConf = new SparkConf()
       .setAppName("Recommend")
@@ -18,15 +21,17 @@ object test {
     println("master=" + sc.master)
 
     println("----------Setting Up Data Path----------")
-    val dataPath = "C:\\Users\\samue\\recommendationsystem\\steam-dataset\\games.csv" // local for test
-    val modelPath = "C:\\Users\\samue\\recommendationsystem\\out" // local for test
+    val dataPath = "/Users/leonardovincenzi/IdeaProjects/recommendationsystem/steam-dataset/games.csv" // local for test
+    val modelPath = "/Users/leonardovincenzi/IdeaProjects/recommendationsystem/out" // local for test
+
+
     //val checkpointPath = "home/caihao/Downloads/ml-100k/checkpoint/" // local for test
     //val dataPath = "hdfs://localhost:9000/user/caihao/movie/u.item" // HDFS
     //val modelPath = "hdfs://localhost:9000/user/caihao/movie/ALSmodel" // HDFS
     //val checkpointPath = "hdfs://localhost:9000/user/caihao/checkpoint/" // HDFS
     //sc.setCheckpointDir(checkpointPath) // checkpoint directory (to avoid stackoverflow error)
 
-    def prepareData(sc: SparkContext, dataPath:String): RDD[(Int, String)] ={
+    def prepareData(sc: SparkContext, dataPath: String): RDD[(Int, String)] = {
       println("Loading Data......")
       // reads data from dataPath into Spark RDD.
       val itemRDD: RDD[String] = sc.textFile(dataPath)
@@ -48,7 +53,7 @@ object test {
       finally {}
     }
 
-    def recommend(model: Option[MatrixFactorizationModel], movieTitle:RDD[(Int, String)], arg1: String, arg2: String): Unit ={
+    def recommend(model: Option[MatrixFactorizationModel], movieTitle: RDD[(Int, String)], arg1: String, arg2: String): Unit = {
       if (arg1 == "--U") {
         recommendMovies(model.get, movieTitle, arg2.toInt)
       }
@@ -59,26 +64,56 @@ object test {
 
     def recommendMovies(model: MatrixFactorizationModel, movieTitle: RDD[(Int, String)], inputUserID: Int): Unit = {
       val recommendP = model.recommendProducts(inputUserID, 10)
-      println(s"Recommending the following games for user ${inputUserID.toString}:")
-      recommendP.foreach(p => println(s"user: ${p.user}, recommended movie: ${movieTitle.lookup(p.product).mkString}, rating: ${p.rating}"))
+      val out = new StringBuilder()
+      out.append(s"Recommending the following games for user \n ${inputUserID.toString}:")
+      recommendP.foreach(p => {
+        val gameTitle = movieTitle.lookup(p.product).mkString
+        out.append(s"User: ${p.user}, recommended game: $gameTitle, rating: ${p.rating}\n")
+      })
+
+      out.append(s"\n")
+      saveRecommendations(out, "user", inputUserID.toString)
     }
 
-    def recommendUsers(model: MatrixFactorizationModel, movieTitle: RDD[(Int, String)], inputMovieID: Int): Unit = {
-      val recommendU = model.recommendUsers(inputMovieID, 10)
-      println(s"Recommending the following users for game ${inputMovieID.toString}:")
-      recommendU.foreach(u => println(s"movie: ${movieTitle.lookup(u.product).mkString}, recommended user: ${u.user}, rating: ${u.rating}"))
+    def recommendUsers(model: MatrixFactorizationModel, movieTitle: RDD[(Int, String)], inputGameID: Int): Unit = {
+      val recommendU = model.recommendUsers(inputGameID, 10)
+      val out = new StringBuilder()
+      out.append(s"Recommending the following users for game ${inputGameID.toString}:")
+      println(s"Recommending the following users for game ${inputGameID.toString}:")
+      recommendU.foreach(u => println(s"Game: ${movieTitle.lookup(u.product).mkString}, recommended user: ${u.user}, rating: ${u.rating}"))
+      recommendU.foreach(u => {
+        val gameTitle = movieTitle.lookup(u.product).mkString
+        out.append(s"Game: $gameTitle, recommended user: ${u.user}, rating: ${u.rating}\n")
+      })
+
+      out.append(s"\n")
+      saveRecommendations(out, "game", inputGameID.toString)
     }
 
-    println("---------Preparing Data---------")
-    val movieTitle: RDD[(Int, String)] = prepareData(sc, dataPath)
-    //movieTitle.checkpoint() // checkpoint data to avoid stackoverflow error
+    def saveRecommendations(out: StringBuilder, out_type: String, id: String): Unit = {
+      // Crea il file
+      val dir = modelPath
+      val outputFile = new File(dir, "results.txt")
+      val writer = new PrintWriter(outputFile)
 
-    println("---------Loading Model---------")
-    val model = loadModel(sc, modelPath)
+      // Scrivi il contenuto di "out" nel file
+      try {
+        writer.write(out.toString())
+      } finally {
+        writer.close()
+      }
 
-    println("---------Recommend---------")
-    recommend(model, movieTitle, recommendType, inputID)
+      println("---------Preparing Data---------")
+      val movieTitle: RDD[(Int, String)] = prepareData(sc, dataPath)
+      //movieTitle.checkpoint() // checkpoint data to avoid stackoverflow error
 
-    sc.stop()
+      println("---------Loading Model---------")
+      val model = loadModel(sc, modelPath)
+
+      println("---------Recommend---------")
+      recommend(model, movieTitle, recommendType, inputID)
+
+      sc.stop()
+    }
   }
 }
