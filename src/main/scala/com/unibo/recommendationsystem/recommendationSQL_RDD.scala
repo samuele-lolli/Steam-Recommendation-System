96b6d8db7f8652d51bb5a899eb3bc28e8484c286
Sam <samuelelolliprivato@gmail.com>
2024-06-14 05:34:30 +0200

    improve performance on RDD, SQL/RDD amd mllib version

--- src/main/scala/com/unibo/recommendationsystem/recommendationSQL_RDD.scala
+++ src/main/scala/com/unibo/recommendationsystem/recommendationSQL_RDD.scala
@@ -6,10 +6,8 @@ import org.apache.spark.sql.functions._
 
 object recommendationSQL_RDD {
   def main(args: Array[String]): Unit = {
-
-    //Initialize SparkSession
-    val spark = SparkSession
-      .builder
+    // Initialize SparkSession
+    val spark = SparkSession.builder
       .appName("recommendationsystem")
       .config("spark.master", "local[*]")
       .getOrCreate()
@@ -19,17 +17,17 @@ object recommendationSQL_RDD {
 
     val tPreProcessingI = System.nanoTime()
 
-    // Load dataset as DataFrame
+    // Load datasets
     val dfRec = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(dataPathRec)
     val dfGames = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(dataPathGames)
 
-    // Clean the dataset from useless whitespace and select useful columns
+    // Clean and join datasets
     val cleanMerge = dfRec.select("app_id", "user_id", "is_recommended")
       .join(dfGames.select("app_id", "title"), Seq("app_id"))
       .withColumn("title", lower(trim(regexp_replace(col("title"), "\\s+", " "))))
       .cache()
 
-    // Tokenization of titles on whitespaces and aggregation by user ID
+    // Tokenize titles and aggregate by user ID
     val filteredData = cleanMerge
       .withColumn("words", split(col("title"), "\\s+"))
       .groupBy("user_id")
@@ -37,41 +35,35 @@ object recommendationSQL_RDD {
       .filter(size(col("words")) >= 20)
       .cache()
 
-    // Explode the aggregated data for TF-IDF calculation
+    // Explode aggregated data for TF-IDF calculation
     val explodedDF = filteredData.withColumn("word", explode(col("words"))).select("user_id", "word").cache()
 
     val tPreProcessingF = System.nanoTime()
 
     val tTFIDFI = System.nanoTime()
 
-    // Calculate the total number of words associated with each unique user in the dataset
+    // Calculate total number of words for each user
     val wordsPerUser = explodedDF.groupBy("user_id").agg(count("*").alias("total_words"))
 
-    // Calculate the Term Frequency (TF) for each word and user combination
+    // Calculate Term Frequency (TF)
     val tf = explodedDF.groupBy("user_id", "word")
       .agg(count("*").alias("term_count"))
       .join(wordsPerUser, "user_id")
       .withColumn("term_frequency", col("term_count") / col("total_words"))
 
-    // Calculate Document Frequency
+    // Calculate Document Frequency (DF)
     val dfDF = explodedDF.groupBy("word")
       .agg(countDistinct("user_id").alias("document_frequency"))
 
-    // Counting the total number of users.
+    // Calculate total number of users
     val totalDocs = filteredData.count()
 
-    // Calculate the IDF for each word in your RDD
-    val idfRDD = dfDF.rdd.map { row =>
-      val word = row.getString(0) // Assuming word is at index 0
-      val docFreq = row.getLong(1) // Assuming document_frequency is at index 1
-      val idf = math.log(totalDocs.toDouble / docFreq)
-      (word, idf)
-    }
+    // Calculate IDF for each word using SQL
+    dfDF.createOrReplaceTempView("dfDF")
+    val idfDF = spark.sql(s"""SELECT word, log($totalDocs / document_frequency) AS idf FROM dfDF""")
 
-    import spark.implicits._ // For toDF() method
-
-    // Join the DataFrames on the 'word' column
-    val tfidfDF = tf.join(idfRDD.toDF("word", "idf"), "word")
+    // Join TF and IDF to get TF-IDF
+    val tfidfDF = tf.join(idfDF, "word")
       .withColumn("tf_idf", col("term_frequency") * col("idf"))
       .select("user_id", "word", "tf_idf")
 
@@ -81,48 +73,44 @@ object recommendationSQL_RDD {
       .withColumn("word_tfidf_map", map_from_arrays(col("words"), col("tf_idf_values")))
 
     val tTFIDFF = System.nanoTime()
-
     val tCosineSimilarityI = System.nanoTime()
 
-    //Convert in a RDD[(Int, Map[String, Double])]
+    // Convert to RDD[(Int, Map[String, Double])]
     val userSimRDD = preprocessedDF.rdd.map(row => {
       val userId = row.getAs[Int]("user_id")
       val wordTfidfMap = row.getAs[Map[String, Double]]("word_tfidf_map")
       (userId, wordTfidfMap)
     })
 
-    // Input: two vectors as a map of words and weights
-    // Output: cosine similarity
+    // Function to compute cosine similarity
     def computeCosineSimilarity(vector1: Map[String, Double], vector2: Map[String, Double]): Double = {
       def dotProduct(v1: Map[String, Double], v2: Map[String, Double]): Double = {
         v1.foldLeft(0.0) { case (acc, (key, value)) =>
-          v2.get(key).map(value * _).getOrElse(0.0) + acc // Handle potential missing keys and type errors
+          v2.get(key).map(value * _).getOrElse(0.0) + acc
         }
       }
 
-      // Calculate vector magnitude (length)
       def magnitude(vector: Map[String, Double]): Double = {
         math.sqrt(vector.values.map(value => value * value).sum)
       }
 
-      // Calculate cosine similarity
       dotProduct(vector1, vector2) / (magnitude(vector1) * magnitude(vector2))
     }
 
     val targetUser = 2591067
 
-    // Get users similar to the target
+    // Get similar users to the target user
     def getSimilarUsers(userId: Int, tfidfValues: RDD[(Int, Map[String, Double])]): Array[(Int, Double)] = {
       val userGames = tfidfValues.lookup(userId).head
       tfidfValues.filter(_._1 != userId).map {
         case (otherUserId, otherUserGames) => (otherUserId, computeCosineSimilarity(userGames, otherUserGames))
-      }.collect().sortBy(-_._2).take(3)
+      }.collect().sortBy(-_._2).take(10)
     }
 
-    // Get recommendations for target users, based on previously calculated TF-IDF values
     val recommendedUsers = getSimilarUsers(targetUser, userSimRDD)
+    val tCosineSimilarityF = System.nanoTime()
 
-    println("recommendedUsers Top 3")
+    println("Top 3 similar users:")
     recommendedUsers.foreach(println)
 
     /*
@@ -131,10 +119,9 @@ object recommendationSQL_RDD {
     (4509885,0.7186991307198306)
     */
 
-    val tCosineSimilarityF = System.nanoTime()
-
     val tFinalRecommendI = System.nanoTime()
 
+    import spark.implicits._
     // Extract games recommended by the target user
     val titlesPlayedByTargetUser = cleanMerge
       .filter(col("user_id") === targetUser)
@@ -144,13 +131,13 @@ object recommendationSQL_RDD {
       .collect()
 
     // Extract relevant user IDs from recommendations
-    val userIdsToFind = recommendedUsers.map(_._1).toSet
+    val userIdsToFind = recommendedUsers.take(3).map(_._1).toSet
 
-    // Filter datasetDF to remove already played games
+    // Filter dataset to remove already played games and aggregate recommendations
     val finalRecommendations = cleanMerge
-      .filter(col("user_id").isin(userIdsToFind.toSeq: _*) &&
-        !col("title").isin(titlesPlayedByTargetUser: _*) &&
-        col("is_recommended") === true)
+      .filter(col("user_id").isin(userIdsToFind.toSeq: _*)
+        && !col("title").isin(titlesPlayedByTargetUser: _*)
+        && col("is_recommended") === true)
       .groupBy("app_id", "title")
       .agg(collect_list("user_id").alias("users"))
 
@@ -159,27 +146,25 @@ object recommendationSQL_RDD {
     finalRecommendations.take(100).foreach(println)
 
     /*
-    +-------+--------------------+----------+
-    | app_id|               title|     users|
-    +-------+--------------------+----------+
-    |1085660|           destiny 2|[14044364]|
-    |1172470|       apex legends™|[14044364]|
-      | 307690|sleeping dogs: de...|[14044364]|
-      |1267910|         melvor idle|[14044364]|
-      |1227890|     summer memories| [4509885]|
-      |1126290|                lost|[14044364]|
-    |1153430|           love wish| [4509885]|
-      |1109460|there is no greendam| [4509885]|
-      |1146630|      yokai's secret| [4509885]|
-      +-------+--------------------+----------+
-  */
-    // Calculating execution times
-    println("\n\nExecution time(preprocessing):\t"+ (tPreProcessingF-tPreProcessingI)/1000000 + "ms\n")
-    println("\n\nExecution time(Tf-Idf calculation):\t"+ (tTFIDFF-tTFIDFI)/1000000 + "ms\n")
-    println("\n\nExecution time(Cosine similarity calculation):\t"+ (tCosineSimilarityF-tCosineSimilarityI)/1000000 + "ms\n")
-    println("\n\nExecution time(final recommendation):\t"+ (tFinalRecommendF-tFinalRecommendI)/1000000 + "ms\n")
-    println("\n\nExecution time(total):\t"+ (tFinalRecommendF-tPreProcessingI)/1000000 + "ms\n")
+    [1085660,destiny 2,WrappedArray(14044364)]
+    [1172470,apex legends™,WrappedArray(14044364)]
+    [307690,sleeping dogs: definitive edition,WrappedArray(14044364)]
+    [1267910,melvor idle,WrappedArray(14044364)]
+    [1227890,summer memories,WrappedArray(4509885)]
+    [1126290,lost,WrappedArray(14044364)]
+    [1153430,love wish,WrappedArray(4509885)]
+    [1109460,there is no greendam,WrappedArray(4509885)]
+    [1146630,yokai's secret,WrappedArray(4509885)]
+    */
+
+    // Calculate and display execution times
+    println(s"\n\nExecution time (preprocessing): ${(tPreProcessingF - tPreProcessingI) / 1000000} ms")
+    println(s"\n\nExecution time (TF-IDF calculation): ${(tTFIDFF - tTFIDFI) / 1000000} ms")
+    println(s"\n\nExecution time (cosine similarity calculation): ${(tCosineSimilarityF - tCosineSimilarityI) / 1000000} ms")
+    println(s"\n\nExecution time (final recommendation): ${(tFinalRecommendF - tFinalRecommendI) / 1000000} ms")
+    println(s"\n\nExecution time (total): ${(tFinalRecommendF - tPreProcessingI) / 1000000} ms")
 
     spark.stop()
   }
 }
+
