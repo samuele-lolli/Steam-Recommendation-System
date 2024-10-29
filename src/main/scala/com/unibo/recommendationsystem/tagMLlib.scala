91011a21e002ff324fe1292d2d8f33b375b4de17
Leonardo Vincenzi <leovincenzi98@gmail.com>
2024-10-29 16:09:36 +0100

    Change target user

--- src/main/scala/com/unibo/recommendationsystem/tagMLlib.scala
+++ src/main/scala/com/unibo/recommendationsystem/tagMLlib.scala
@@ -14,6 +14,7 @@ object tagMLlib {
     // Initialize SparkSession
     val spark = SparkSession.builder
       .appName("recommendationsystem")
+      .config("spark.master", "local[*]")
 
       .getOrCreate()
 
@@ -53,9 +54,9 @@ object tagMLlib {
     ))
 
     // Paths for datasets
-    val dataPathRec = "gs://dataproc-staging-us-central1-534461255477-conaqzw0/data/recommendations.csv"
-    val dataPathGames = "gs://dataproc-staging-us-central1-534461255477-conaqzw0/data/games.csv"
-    val metadataPath = "gs://dataproc-staging-us-central1-534461255477-conaqzw0/data/games_metadata.json"
+    val dataPathRec = "/Users/leonardovincenzi/IdeaProjects/recommendationsystem/steam-dataset/recommendations.csv"
+    val dataPathGames = "/Users/leonardovincenzi/IdeaProjects/recommendationsystem/steam-dataset/games.csv"
+    val metadataPath = "/Users/leonardovincenzi/IdeaProjects/recommendationsystem/steam-dataset/games_metadata.json"
 
     val tPreProcessingI = System.nanoTime()
 
@@ -111,7 +112,7 @@ object tagMLlib {
     val dfWithDenseFeatures = rescaledData.withColumn("dense_features", denseVector(col("features")))//.persist(StorageLevel.MEMORY_AND_DISK)
 
     // Target user ID
-    val targetUser = 2591067
+    val targetUser = 4893896
 
     def cosineSimilarity(targetVector: Vector): UserDefinedFunction = udf { (otherVector: Vector) =>
 
@@ -137,9 +138,9 @@ object tagMLlib {
     usersSimilarity.take(3).foreach(println)
     println("Top 3 similar users:")
     /*
-[65064,0.8793965595063066]
-[13498880,0.8783593100136686]
-[7002264,0.8755346657835146]
+[8971360,0.8591792924376707]
+[13271546,0.8443858670280873]
+[11277999,0.8432720374293458]
      */
 
 
@@ -160,7 +161,7 @@ object tagMLlib {
       .agg(collect_list("user_id").alias("users"))
 
 
-    finalRecommendations.take(100).foreach(println)
+    finalRecommendations.show(finalRecommendations.count.toInt, truncate = false)
 
     val tFinalRecommendF = System.nanoTime()
 
@@ -173,121 +174,60 @@ object tagMLlib {
 
     //LOCALE
     /*
-    [310950,Street Fighter V,WrappedArray(7002264, 65064)]
-[1901340,Ero Manager,WrappedArray(65064)]
-[285190,Warhammer 40000: Dawn of War III,WrappedArray(7002264)]
-[899970,NEKOPARA Extra,WrappedArray(7002264)]
-[1504020,Mosaique Neko Waifus 4,WrappedArray(7002264, 13498880)]
-[966330,Flower,WrappedArray(13498880)]
-[1592110,Spirit of the Island,WrappedArray(65064)]
-[1205960,My Cute Fuhrer,WrappedArray(7002264, 65064)]
-[1431610,Prison Simulator Prologue,WrappedArray(13498880)]
-[678950,DRAGON BALL FighterZ,WrappedArray(65064)]
-[1135300,King's Bounty II,WrappedArray(65064, 7002264)]
-[1211360,NEOMORPH,WrappedArray(65064, 13498880)]
-[407330,Sakura Dungeon,WrappedArray(7002264)]
-[1732740,Sakura Hime,WrappedArray(65064)]
-[469820,Genital Jousting,WrappedArray(13498880)]
-[489020,iGrow Game,WrappedArray(7002264)]
-[372330,Beauty Bounce,WrappedArray(7002264)]
-[1205260,Defeated Girl,WrappedArray(65064)]
-[1460040,Love Fantasy,WrappedArray(65064, 13498880)]
-[521540,This World Unknown,WrappedArray(7002264)]
-[870780,Control Ultimate Edition,WrappedArray(65064)]
-[282070,This War of Mine,WrappedArray(65064)]
-[1101450,Miss Neko,WrappedArray(13498880, 7002264, 65064)]
-[1153430,Love wish,WrappedArray(7002264, 65064, 13498880)]
-[528230,SYNTHETIK: Legion Rising,WrappedArray(65064)]
-[1282410,Hard West 2,WrappedArray(65064)]
-[998930,Seek Girl,WrappedArray(65064)]
-[1148510,Pretty Angel,WrappedArray(65064, 13498880)]
-[33100,Alien Shooter,WrappedArray(13498880)]
-[404180,Club Life,WrappedArray(7002264)]
-[606800,Startup Company,WrappedArray(7002264)]
-[1201230,Immortal Life,WrappedArray(65064)]
-[1508680,Love n War: Warlord by Chance,WrappedArray(65064)]
-[1157750,CATGIRL LOVER 2,WrappedArray(65064, 7002264)]
-[1191210,Seek Girl Ⅲ,WrappedArray(65064, 13498880)]
-[728740,Sniper Elite V2 Remastered,WrappedArray(65064)]
-[949290,Winkeltje: The Little Shop,WrappedArray(65064)]
-[1677180,Neko Hacker Plus,WrappedArray(7002264)]
-[577670,Demolish & Build 2018,WrappedArray(13498880)]
-[1157390,King Arthur: Knight's Tale,WrappedArray(65064)]
-[680420,OUTRIDERS,WrappedArray(65064)]
-[1252560,Love Breakout,WrappedArray(13498880)]
-[1299120,Mosaique Neko Waifus 2,WrappedArray(13498880, 65064, 7002264)]
-[960990,Beyond: Two Souls,WrappedArray(65064)]
-[1069740,Seen,WrappedArray(7002264)]
-[1426110,Love n Dream: Virtual Happiness,WrappedArray(65064, 13498880)]
-[234270,Ken Follett's The Pillars of the Earth,WrappedArray(65064)]
-[1543030,Sword and Fairy 7,WrappedArray(65064)]
-[810670,NALOGI,WrappedArray(7002264)]
-[400910,Rabi-Ribi,WrappedArray(13498880, 7002264)]
-[970830,The Dungeon Of Naheulbeuk: The Amulet Of Chaos,WrappedArray(65064)]
-[691770,Eiyu*Senki – The World Conquest,WrappedArray(65064)]
-[1182760,Starlight,WrappedArray(65064, 13498880)]
-[384180,Prominence Poker,WrappedArray(13498880)]
-[601220,Zup! F,WrappedArray(7002264)]
-[1212830,Seek Girl Ⅳ,WrappedArray(65064)]
-[1083210,符文女孩/Rune Girl,WrappedArray(65064)]
-[1709500,Adorable Witch 2,WrappedArray(7002264)]
-[979690,The Ascent,WrappedArray(65064)]
-[1123830,Farm Manager 2021,WrappedArray(65064)]
-[382560,Hot Lava,WrappedArray(13498880)]
-[367450,Poly Bridge,WrappedArray(13498880)]
-[627270,Injustice™ 2,WrappedArray(65064)]
-[1060670,Taboos: Cracks,WrappedArray(65064, 13498880)]
-[654820,Akin Vol 2,WrappedArray(13498880)]
-[1146630,Yokai's Secret,WrappedArray(65064)]
-[429580,A Wild Catgirl Appears!,WrappedArray(7002264)]
-[1913490,Adorable Witch 3,WrappedArray(65064)]
-[1277940,Kinkoi: Golden Loveriche,WrappedArray(7002264)]
-[335190,200% Mixed Juice!,WrappedArray(7002264)]
-[1457550,Melody/心跳旋律,WrappedArray(13498880)]
-[356400,Thumper,WrappedArray(13498880)]
-[1454400,Cookie Clicker,WrappedArray(7002264)]
-[1812060,Yokai Art: Night Parade of One Hundred Demons,WrappedArray(65064)]
-[882100,XCOM®: Chimera Squad,WrappedArray(65064)]
-[1303740,Love n Dream,WrappedArray(65064, 13498880)]
-[365450,Hacknet,WrappedArray(13498880)]
-[1591530,SAMURAI WARRIORS 5,WrappedArray(65064)]
-[1605010,Adorable Witch,WrappedArray(7002264)]
-[289130,ENDLESS™ Legend,WrappedArray(65064)]
-[1070330,Russian Life Simulator,WrappedArray(7002264)]
-[1393410,Seek Girl V,WrappedArray(13498880, 65064)]
-[1509090,Seek Girl Ⅷ,WrappedArray(65064)]
-[102600,Orcs Must Die!,WrappedArray(13498880)]
-[1149660,Seek Girl Ⅱ,WrappedArray(65064, 13498880)]
-[1274300,Cyber Agent,WrappedArray(65064, 13498880, 7002264)]
-[673950,Farm Together,WrappedArray(13498880)]
-[802870,The Ditzy Demons Are in Love With Me,WrappedArray(13498880)]
-[985760,3D Custom Lady Maker,WrappedArray(65064)]
-[1226530,Trap Legend,WrappedArray(7002264)]
-[1238000,Mass Effect™: Andromeda Deluxe Edition,WrappedArray(65064)]
-[1543080,Cute Honey 3,WrappedArray(7002264)]
-[1548820,Happy Puzzle,WrappedArray(13498880, 65064)]
-[994730,Banner of the Maid,WrappedArray(65064)]
-[1615290,Ravenous Devils,WrappedArray(65064)]
-[1502230,Tower of Waifus,WrappedArray(13498880, 65064)]
-[914710,Cat Quest II,WrappedArray(13498880)]
-[606880,GreedFall,WrappedArray(65064)]
-[1140440,OMON Simulator,WrappedArray(7002264)]
-[1359650,The Last of Waifus,WrappedArray(7002264)]
++-------+-------------------------------------------------------+--------------------+
+|app_id |title                                                  |users               |
++-------+-------------------------------------------------------+--------------------+
+|35450  |Red Orchestra 2: Heroes of Stalingrad with Rising Storm|[8971360, 13271546] |
+|1072040|Panzer Corps 2                                         |[11277999]          |
+|4700   |Total War: MEDIEVAL II – Definitive Edition            |[8971360]           |
+|304930 |Unturned                                               |[8971360]           |
+|268400 |Panzer Corps Gold                                      |[11277999]          |
+|555570 |Infestation: The New Z                                 |[13271546]          |
+|34030  |Total War: NAPOLEON – Definitive Edition               |[13271546]          |
+|41700  |S.T.A.L.K.E.R.: Call of Pripyat                        |[13271546]          |
+|312450 |Order of Battle: World War II                          |[8971360]           |
+|301640 |Zombie Army Trilogy                                    |[13271546]          |
+|549080 |Tank Warfare: Tunisia 1943                             |[13271546]          |
+|228200 |Company of Heroes                                      |[11277999]          |
+|1369370|Combat Mission Shock Force 2                           |[13271546]          |
+|736220 |Post Scriptum                                          |[13271546]          |
+|494840 |UBOAT                                                  |[13271546]          |
+|312980 |Graviteam Tactics: Mius-Front                          |[13271546]          |
+|242860 |Verdun                                                 |[8971360, 13271546] |
+|995460 |Miracle Snack Shop                                     |[13271546]          |
+|447820 |Day of Infamy                                          |[13271546]          |
+|302670 |Call to Arms                                           |[13271546]          |
+|244450 |Men of War: Assault Squad 2                            |[11277999, 8971360] |
+|418460 |Rising Storm 2: Vietnam                                |[8971360, 13271546] |
+|1200   |Red Orchestra: Ostfront 41-45                          |[11277999, 13271546]|
+|485980 |Syrian Warfare                                         |[13271546]          |
+|201810 |Wolfenstein: The New Order                             |[13271546]          |
+|572410 |Steel Division: Normandy 44                            |[13271546]          |
+|231430 |Company of Heroes 2                                    |[13271546]          |
+|350080 |Wolfenstein: The Old Blood                             |[13271546]          |
+|222880 |Insurgency                                             |[13271546]          |
+|712100 |A Total War Saga: THRONES OF BRITANNIA                 |[13271546]          |
+|259080 |Just Cause 2: Multiplayer Mod                          |[11277999]          |
+|251060 |Wargame: Red Dragon                                    |[13271546]          |
+|363680 |Battlefleet Gothic: Armada                             |[13271546]          |
+|1238060|Dead Space™ 3                                          |[13271546]          |
++-------+-------------------------------------------------------+--------------------+
 
 
-Execution time (preprocessing): 1909 ms
+
+Execution time (preprocessing): 2227 ms
 
 
-Execution time (Tf-Idf calculation): 217026 ms
+Execution time (Tf-Idf calculation): 192343 ms
 
 
-Execution time (cosine similarity calculation): 30308 ms
+Execution time (cosine similarity calculation): 16422 ms
 
 
-Execution time (final recommendation): 20087 ms
+Execution time (final recommendation): 25750 ms
 
 
-Execution time (total): 269332 ms
+Execution time (total): 236744 ms
      */
 
     //CLUSTER-PRIMO
