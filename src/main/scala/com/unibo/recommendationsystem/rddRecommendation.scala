1be2e0c073209414fb1246db0e2410389bd5871c
gbekss <gbekss@gmail.com>
2024-11-07 17:57:43 +0100

    rddRecommendation now filters target user's games

--- src/main/scala/com/unibo/recommendationsystem/rddRecommendation.scala
+++ src/main/scala/com/unibo/recommendationsystem/rddRecommendation.scala
@@ -13,19 +13,49 @@ class rddRecommendation (spark: SparkSession, dataRec: Dataset[Row], dataGames: 
   def recommend(targetUser: Int): Unit = {
     // Time the preprocessing of data
     println("Preprocessing data...")
+    //val tPreProcessingI = System.nanoTime()
     val (mergedRdd, explodedRDD, gamesData) = timeUtils.time(preprocessData(), "Preprocessing Data", "RDD")
+    //val (mergedRdd, explodedRDD, gamesData) = preprocessData()
+    //val tPreProcessingF = System.nanoTime()
 
     // Time the TF-IDF calculation
     println("Calculate term frequency and inverse document frequency...")
+    //val tTFIDFI = System.nanoTime()
     val tfidfValues = timeUtils.time(calculateTFIDF(explodedRDD), "Calculating TF-IDF", "RDD")
+    //val tfidfValues = calculateTFIDF(explodedRDD)
+    //val tTFIDFF = System.nanoTime()
 
     // Time the similarity computation
     println("Calculate cosine similarity to get similar users...")
+    //val tCosineSimilarityI = System.nanoTime()
     val topUsersSimilarity = timeUtils.time(getSimilarUsers(targetUser, tfidfValues), "Getting Similar Users", "RDD")
+    //val topUsersSimilarity = getSimilarUsers(targetUser, tfidfValues)
+    //val tCosineSimilarityF = System.nanoTime()
 
     // Time the recommendation generation
-    println("\nCalculate final recommendation...")
+    println("Calculate final recommendation...")
+    //val tFinalRecommendI = System.nanoTime()
     timeUtils.time(getRecommendation(mergedRdd, topUsersSimilarity, gamesData, targetUser), "Generating Recommendations","RDD")
+
+   // timeUtils.flushLogsToGCS(spark)
+
+    //getRecommendation(mergedRdd, topUsersSimilarity, gamesData, targetUser)
+    //val tFinalRecommendF = System.nanoTime()
+
+/*
+    println(s"\n\nExecution time(preprocessing): ${(tPreProcessingF - tPreProcessingI) / 1000000}ms\n")
+    println(s"\n\nExecution time(Tf-Idf calculation): ${(tTFIDFF - tTFIDFI) / 1000000}ms\n")
+    println(s"\n\nExecution time(Cosine similarity calculation): ${(tCosineSimilarityF - tCosineSimilarityI) / 1000000}ms\n")
+    println(s"\n\nExecution time(final recommendation): ${(tFinalRecommendF - tFinalRecommendI) / 1000000}ms\n")
+    println(s"\n\nExecution time(total): ${(tFinalRecommendF - tPreProcessingI) / 1000000}ms\n")
+
+ */
+
+    /*
+    Elapsed time for Generating Recommendations:	41423ms (41423732625ns)
+     */
+    //Total time of execution: 427211ms
+
   }
 
   private def preprocessData() : (RDD[(Int, String, String)], RDD[(String, String)], RDD[(Int, String)]) = {
@@ -60,6 +90,7 @@ class rddRecommendation (spark: SparkSession, dataRec: Dataset[Row], dataGames: 
       .filter { case (_, tag, _) => tag.nonEmpty }  // Filter out empty tags
       .persist(StorageLevel.MEMORY_AND_DISK)
 
+
     // Step 4: Aggregate data by user using a higher-order function to pass a filtering condition
     def aggregateByUser[T](rdd: RDD[(String, Array[String])], filterFunc: Array[String] => Boolean): RDD[(String, Array[String])] = {
       rdd.filter { case (_, words) => filterFunc(words) }
@@ -81,7 +112,10 @@ class rddRecommendation (spark: SparkSession, dataRec: Dataset[Row], dataGames: 
 
     val explodedRDD = explodeRDD(filteredAggregateDataRDD, (userId, words) => words.map(word => (userId, word)))
 
+
     val gamesTitlesRDD = dataGames.rdd.map(row => (row.getAs[Int]("app_id"), row.getAs[String]("title")))
+
+
 
     (mergedRDD, explodedRDD, gamesTitlesRDD)
   }
@@ -155,41 +189,44 @@ class rddRecommendation (spark: SparkSession, dataRec: Dataset[Row], dataGames: 
       math.sqrt(vector.values.map(value => value * value).sum)
     }
 
-// Step 8: Higher-order function to get similar users
-def getSimilarUsers[T : ClassTag](
-                                   targetUser: Int,
-                                   tfidfValues: RDD[(String, Map[String, Double])],
-                                   similarityFunc: (Map[String, Double], Map[String, Double]) => Double
-                                 ): Array[(String, Double)] = {
+    // Step 8: Higher-order function to get similar users
 
-  // Filter to get the target user's games
+    def getSimilarUsers[T : ClassTag](
+                                      targetUser: Int,
+                                      tfidfValues: RDD[(String, Map[String, Double])],
+                                      similarityFunc: (Map[String, Double], Map[String, Double]) => Double
+                                    ): Array[(String, Double)] = {
 
-  val targetUserGamesRDD = tfidfValues
-    .filter { case (userId, _) => userId == targetUser.toString }
-    .map { case (_, gameVector) => gameVector }
+      // Filter to get the target user's games
 
-  // Step 2: Check if the target user has any games
-  if (!targetUserGamesRDD.isEmpty()) {
-    // Step 3: Collect the target user's games (should be a single map)
-    val targetUserGames = targetUserGamesRDD.collect().head
+      val targetUserGamesRDD = tfidfValues
+        .filter { case (userId, _) => userId == targetUser.toString }
+        .map { case (_, gameVector) => gameVector }
+      //println(s"Time for filtering: ${(tFilterEnd - tFilterStart) / 1e9} seconds")
+
+      // Step 2: Check if the target user has any games
+      if (!targetUserGamesRDD.isEmpty()) {
+        // Step 3: Collect the target user's games (should be a single map)
+        val targetUserGames = targetUserGamesRDD.collect().head
+        // println(s"Time for collecting: ${(tCollectEnd - tCollectStart) / 1e9} seconds")
 
 
-    // Step 3: Compute similarity for other users
-    val similarUsers = tfidfValues
-      .filter(_._1 != targetUser.toString) // Filter out the target user
-      .map { case (otherUserId, otherUserGames) =>
-        (otherUserId, similarityFunc(targetUserGames, otherUserGames))
+        // Step 3: Compute similarity for other users
+        val similarUsers = tfidfValues
+          .filter(_._1 != targetUser.toString) // Filter out the target user
+          .map { case (otherUserId, otherUserGames) =>
+            (otherUserId, similarityFunc(targetUserGames, otherUserGames))
+          }
+          .collect() // Collect the results to the driver
+          .sortBy(-_._2) // Sort by similarity (descending)
+          .take(3) // Take top 10 similar users
+
+        similarUsers // Return the list of similar users
+      } else {
+        // Step 4: Return an empty array if the target user has no games
+        Array.empty[(String, Double)]
       }
-      .collect() // Collect the results to the driver
-      .sortBy(-_._2) // Sort by similarity (descending)
-      .take(3) // Take top 10 similar users
-
-    similarUsers // Return the list of similar users
-  } else {
-    // Step 4: Return an empty array if the target user has no games
-    Array.empty[(String, Double)]
-  }
-}
+    }
 
     // Use cosine similarity logic
     val cosineSimilarity = (v1: Map[String, Double], v2: Map[String, Double]) =>
@@ -208,25 +245,22 @@ def getSimilarUsers[T : ClassTag](
                                ): Unit = {
 
     val appIdsPlayedByTargetUser = mergedRdd
-      .filter { case (_, _, user) => user == targetUser.toString }
-      .map(_._1)
-      .distinct()
-      .collect()
-      .toSet
+      .filter { case (_, _, user) => user == targetUser.toString }  // Filter by targetUser
+      .map(_._1)  // Extract appId
+      .distinct()  // Get unique appIds
+      .collect()   // Collect appIds into an array
+      .toSet       // Convert to a Set for easy lookup
 
     // Convert dfGames to an RDD of (appId, title)
 
     // Filter dfGamesRDD by appId and extract titles
     val titlesPlayedByTargetUser = gamesData
-      .filter { case (appId, _) => appIdsPlayedByTargetUser.contains(appId) }
-      .map(_._2)
-      .distinct()
-      .collect()
-      .toSet
+      .filter { case (appId, _) => appIdsPlayedByTargetUser.contains(appId) }  // Filter by appId in the set
+      .map(_._2)  // Extract the titles
+      .distinct() // Ensure unique titles
+      .collect()  // Collect the titles into an array
+      .toSet      // Convert to a Set of titles
 
-    // Print games played by the target user
-    println(s"\nGames played by target user $targetUser:")
-    printGamesPlayedByTargetUser(mergedRdd, gamesData, targetUser)
 
     // Use higher-order function for filtering and mapping final recommendations
     def filterAndMap[T: ClassTag](rdd: RDD[(Int, String, String)],
@@ -237,12 +271,15 @@ def getSimilarUsers[T : ClassTag](
 
     val userIdsToFind = topUsersXSimilarity.take(3).map(_._1).toSet
 
+    /*
+    Top 3 similar users
+(8971360,0.88100129281368)
+(9911449,0.8785642563919683)
+(11277999,0.8678767593227635)
+     */
+
     val finalRecommendations = filterAndMap(mergedRdd,
-      { case (appId, tag, user) =>
-        userIdsToFind.contains(user) &&
-          !titlesPlayedByTargetUser.contains(tag) &&
-          !appIdsPlayedByTargetUser.contains(appId)
-      },
+      { case (appId, tag, user) => userIdsToFind.contains(user) && !appIdsPlayedByTargetUser.contains(appId)},
       { case (appId, tag, user) => (appId, tag, user) })
 
     // Step 2: Prepare finalRecommendations by mapping the appId to the title
@@ -268,25 +305,26 @@ def getSimilarUsers[T : ClassTag](
     }
   }
 
-  private def printGamesPlayedByTargetUser(
-                                            mergedRdd: RDD[(Int, String, String)],
-                                            gamesData: RDD[(Int, String)],
-                                            targetUser: Int
-                                          ): Unit = {
-    val appIdsPlayedByTargetUser = mergedRdd
-      .filter { case (_, _, user) => user == targetUser.toString }
-      .map(_._1)
-      .distinct()
-      .collect()
-      .toSet
 
-    val titlesPlayedByTargetUser = gamesData
-      .filter { case (appId, _) => appIdsPlayedByTargetUser.contains(appId) }
-      .map { case (appId, title) => (appId, title) } // Keep both appId and title
-      .collect()
-
-    titlesPlayedByTargetUser.foreach { case (appId, title) =>
-      println(s"appId: $appId, title: $title")
-    }
-  }
 }
+/*
+Execution time(preprocessing): 4669ms
+Execution time(Tf-Idf calculation): 171363ms
+Execution time(Cosine similarity calculation): 126298ms
+Execution time(final recommendation): 7548ms
+Execution time(total): 309878ms
+
+
+Elapsed time for Preprocessing Data:	4164ms (4164205542ns)
+Elapsed time for Calculating TF-IDF:	198220ms (198220372000ns)
+Elapsed time for Getting Similar Users:	134354ms (134354478125ns)
+Elapsed time for Generating Recommendations:	9320ms (9320373250ns)
+
+
+Execution time(preprocessing): 4164ms
+Execution time(Tf-Idf calculation): 198220ms
+Execution time(Cosine similarity calculation): 134355ms
+Execution time(final recommendation): 9323ms
+Execution time(total): 346063ms
+
+ */
