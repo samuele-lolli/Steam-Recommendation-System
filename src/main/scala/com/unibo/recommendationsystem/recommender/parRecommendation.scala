60b6f594037c270c739d12738804ef3178eaa7b4
gbekss <gbekss@gmail.com>
2024-11-20 16:14:41 +0100

    parRecommendation update

--- src/main/scala/com/unibo/recommendationsystem/recommender/parRecommendation.scala
+++ src/main/scala/com/unibo/recommendationsystem/recommender/parRecommendation.scala
@@ -1,6 +1,7 @@
 package com.unibo.recommendationsystem.recommender
 
 import com.unibo.recommendationsystem.utils.timeUtils
+import org.apache.spark
 import org.json4s.DefaultFormats
 import org.json4s.jackson.JsonMethods
 
@@ -8,30 +9,38 @@ import scala.collection.concurrent.TrieMap
 import scala.collection.parallel.{ParMap, ParSeq}
 import scala.io.Source
 import scala.util.Using
+import scala.util.control.Breaks.break
 
 class parRecommendation(dataRecPath: String, dataGamesPath: String, metadataPath: String) {
-
-
-  // Load data
   private val dataRec: Map[Int, Array[Int]] = loadRecommendations(dataRecPath)
   private val dataGames: Map[Int, String] = loadDataGames(dataGamesPath)
   private val metadata: Map[Int, Array[String]] = loadMetadata(metadataPath)
 
-
+  /**
+   * Computes TF-IDF values for all users based on their tags
+   *
+   * @param targetUser Int, The user ID for whom the recommendations are being generated
+   *
+   */
   def recommend(targetUser: Int): Unit = {
     println("Preprocessing data...")
-    val (explodedData, merged) = timeUtils.time(preprocessData(), "Preprocessing Data", "Par")
+    val (userTagsMap, userAppDetails) = timeUtils.time(preprocessData(), "Preprocessing Data", "Par")
     println("Calculate term frequency and inverse document frequency...")
-    val tfidfValuesPar = timeUtils.time(calculateTFIDF(explodedData), "Calculating TF-IDF", "Par")
-      println("Calculate cosine similarity to get similar users...")
+    val tfidfValuesPar = timeUtils.time(calculateTFIDF(userTagsMap), "Calculating TF-IDF", "Par")
+    println("Calculate cosine similarity to get similar users...")
     val topUsersSimilarityPar= timeUtils.time(computeCosineSimilarity(tfidfValuesPar, targetUser), "Getting Similar Users", "Par")
     println("Calculate final recommendation...")
-    timeUtils.time(generateFinalRecommendations(topUsersSimilarityPar, targetUser, merged), "Generating Recommendations", "Par")
+    timeUtils.time(generateFinalRecommendations(topUsersSimilarityPar, targetUser, userAppDetails), "Generating Recommendations", "Par")
   }
 
-
+  /**
+   * Preprocesses the input data to create intermediate scala.collections needed for further calculations.
+   *
+   * @return A tuple of:
+   *         - ParSeq[(Int, String)], map each user with his tags for tf-idf calculation
+   *         - ParSeq[(Int, Int, String, Array[String])], full user-item metadata
+   */
   private def preprocessData(): (ParSeq[(Int, String)], ParSeq[(Int, Int, String, Array[String])]) = {
-
     // Utilizza un ParSeq per memorizzare i dettagli dell'app dell'utente
     val userAppDetails: ParSeq[(Int, Int, String, Array[String])] = dataRec
       .par
@@ -44,7 +53,6 @@ class parRecommendation(dataRecPath: String, dataGamesPath: String, metadataPath
         }
       }.filter(_._4.nonEmpty).toSeq
 
-    // Step 3: Pulisci e formatta i dati uniti
     val cleanMerge: ParSeq[(Int, Int, String, String)] = userAppDetails.map(d =>
       (d._1, d._2, d._3, d._4.mkString(","))
     )
@@ -54,23 +62,54 @@ class parRecommendation(dataRecPath: String, dataGamesPath: String, metadataPath
      (id, cleanedTags)
    }.filter(_._2.nonEmpty)
 
-    // Aggregate data without `groupMapReduce`
     val filteredData: ParSeq[(Int, ParSeq[String])] = cleanedData
-      .groupBy(_._1) // Group by userId
+      .groupBy(_._1)
       .map { case (id, grouped) =>
-        val mergedTags = grouped.flatMap(_._2) // Flatten tags
+        val mergedTags = grouped.flatMap(_._2)
         (id, mergedTags)
       }.toSeq
 
-    // Utilizza `flatMap` per esplodere i dati
     val explodedData: Seq[(Int, String)] = filteredData
       .flatMap { case (userId, tags) => tags.map(tag => (userId, tag)) }.seq
 
     (explodedData.par, userAppDetails)
   }
+  /*
+   * explodedData
+   * (5382866,strategy)
+   * (5382866,classic)
+   * (5382866,funny)
+   * (5382866,turn-based)
+   * (5382866,local multiplayer)
+   * (5382866,classic)
+   * (5382866,2d)
+   * (5382866,retro)
+   * (5382866,online co-op)
+   * (5382866,multiplayer)
+   * (5382866,anime)
+   * (5382866,arcade)
+   */
 
+  /*
+   * userAppDetails
+   * (5382866,272270,Torment: Tides of Numenera, [...tags...] )
+   * (5382866,343340,Tiamat X, [...tags...])
+   * (5382866,233470,Evoland,[...tags...] )
+   * (5382866,599880,Tequila Zombies 3, [...tags...] )
+   * (5382866,357190,ULTIMATE MARVEL VS. CAPCOM 3, [...tags...])
+   * (5382866,544750,SOULCALIBUR VI, [...tags...])
+   * (5382866,1659600,Teenage Mutant Ninja Turtles: The Cowabunga Collection, [...tags...])
+   * (5382866,483420,Adam Wolfe, [...tags...])
+   * (5382866,296770,Real Boxing™, [...tags...])
+ */
 
-  private def calculateTFIDF(explodedList: ParSeq[(Int, String)]): ParMap[Int, Map[String, Double]] = {
+  /**
+   * Computes TF-IDF values for all users based on their tags
+   *
+   * @param userTagsMap ParSeq[(Int, String)], map each user with his tags for tf-idf calculation
+   * @return ParMap[Int, Map[String, Double]], A map where each user ID maps to another map of tags and their respective TF-IDF scores
+   */
+  private def calculateTFIDF(userTagsMap: ParSeq[(Int, String)]): ParMap[Int, Map[String, Double]] = {
     val calculateTF = (userWords: String) => {
       val wordsSplit = userWords.split(",")
       wordsSplit.groupBy(identity).mapValues(_.length.toDouble / wordsSplit.length)
@@ -85,9 +124,9 @@ class parRecommendation(dataRecPath: String, dataGamesPath: String, metadataPath
         .map { case (word, occurrences) => (word, math.log(userCount.toDouble / occurrences.size)) }
     }
 
-    val groupedUserWords: ParMap[Int, String] = explodedList
-      .groupBy(_._1) // Group by user ID
-      .mapValues(_.map(_._2).mkString(",")) // Concatenate all strings for each user
+    val groupedUserWords: ParMap[Int, String] = userTagsMap
+      .groupBy(_._1)
+      .mapValues(_.map(_._2).mkString(","))
 
     val idfValues: Map[String, Double] = calculateIDF(groupedUserWords).seq
 
@@ -97,7 +136,21 @@ class parRecommendation(dataRecPath: String, dataGamesPath: String, metadataPath
         user -> tfValues.map { case (word, tf) => word -> tf * idfValues.getOrElse(word, 0.0) }
       }
   }
+  /*
+   * tfIDFValues
+   * (7785031,Map(multiplayer -> 0.05147607544411006, physics -> 0.2039327116805472, ...))
+   * (7614324,Map(turn-based combat -> 0.058421854161816944, funny -> 0.024955196358551045, ...))
+   * (5107791,Map(1980s -> 0.05896140840799852, indie -> 0.007796680564226543, ...))
+   */
 
+
+  /**
+   * Computes cosine similarity between the target user and all other users
+   *
+   * @param tfidf ParMap[Int, Map[String, Double]], A map where each user ID maps to another map of tags and their respective TF-IDF scores
+   * @param targetUser Int, the ID of the target user
+   * @return A list of the top 3 most similar user IDs
+   */
   private def computeCosineSimilarity(tfidf: ParMap[Int, Map[String, Double]], targetUser: Int): List[Int] = {
 
   def cosineSimilarity(v1: Map[String, Double], v2: Map[String, Double]): Double = {
@@ -106,64 +159,66 @@ class parRecommendation(dataRecPath: String, dataGamesPath: String, metadataPath
     if (magnitude == 0) 0.0 else dotProduct / magnitude
   }
 
-  val targetVector = tfidf(targetUser) // Get the vector for the target user
+  val targetVector = tfidf(targetUser)
   val topUsers = tfidf.seq.view
     .filter { case (key, _) => key != targetUser }
-    .par // Exclude the target user itself
+    .par
     .map { case (key, vector) =>
-      key -> cosineSimilarity(targetVector, vector) // Calculate cosine similarity
+      key -> cosineSimilarity(targetVector, vector)
     }
     .toList // Convert to list for further processing
     .sortBy(-_._2) // Sort by similarity score in descending order
     .take(3) // Take the top 3 most similar users
     .map(_._1)
-  // Extract the user keys (IDs)
+
   topUsers
 }
 
-  private def generateFinalRecommendations(topUsers: List[Int], targetUser: Int, cleanMerge: ParSeq[(Int, Int, String, Array[String])]): Unit = {
-    // Converti cleanMerge in una collezione parallela
+  /**
+   * Generates and prints final game recommendations for a target user based on games played by similar users
+   *
+   * @param topUsers List[Int], list of IDs of the most similar users
+   * @param targetUser Int, the ID of the target user
+   * @param userAppDetails ParSeq[(Int, Int, String, Array[String])], full user-item metadata
+   */
+  private def generateFinalRecommendations(topUsers: List[Int], targetUser: Int, userAppDetails: ParSeq[(Int, Int, String, Array[String])]): Unit = {
     // Step 1: Ottieni tutti i giochi giocati dall'utente target
-    val gamesByTargetUser = cleanMerge.filter(_._1 == targetUser)
+    val gamesByTargetUser = userAppDetails.filter(_._1 == targetUser)
       .map(_._2)
 
     // Step 2: Filtra cleanMerge per ottenere i giochi giocati dai top utenti ma non dall'utente target
-    val filteredGamesGrouped = cleanMerge.filter {
+    val filteredGamesGrouped = userAppDetails.filter {
       case (userId, gameId, _, _) =>
         topUsers.contains(userId) && !gamesByTargetUser.exists(_ == gameId)
     }.groupBy(_._2)
 
    filteredGamesGrouped.foreach { case (gameId, userGames) =>
-     val userIds = userGames.map(_._1).mkString(", ") // Comma-separated user IDs
-     val gameInfo = userGames.head // Any element contains game details
+     val userIds = userGames.map(_._1).mkString(", ")
+     val gameInfo = userGames.head
      println(s"Game ID: $gameId, Title: ${gameInfo._3}, Users: $userIds")
    }
   }
 
 
   def loadRecommendations(path: String): Map[Int, Array[Int]] = {
-    // Use Using to safely manage file resources
+
     Using.resource(Source.fromFile(path)) { source =>
-      // Read lines, drop the header, and convert to a ParArray
+
       val lines = source.getLines().drop(1).toArray.par
 
-      // Use a concurrent map to safely aggregate results in parallel
       val usersRec = TrieMap[String, List[Int]]()
 
-      // Process lines in parallel
       lines.foreach { line =>
         val splitLine = line.split(",")
         val appId = splitLine.head
         val user = splitLine(6)
 
-        // Update the TrieMap manually
         usersRec.synchronized {
           val updatedList = usersRec.getOrElse(user, List()) :+ appId.toInt
           usersRec.put(user, updatedList)
         }
       }
 
-      // Convert the TrieMap to an immutable Map
       usersRec.map { case (user, appIds) =>
         (user.toInt, appIds.reverse.toArray)
       }.toMap
@@ -172,24 +227,22 @@ class parRecommendation(dataRecPath: String, dataGamesPath: String, metadataPath
 
   /** Load game data from CSV */
   private def loadDataGames(path: String): Map[Int, String] = {
-    // Read the file into memory
     val lines = Using.resource(Source.fromFile(path)) { source =>
-      source.getLines().drop(1).toSeq.par // Skip header and load lines into a Seq
+      source.getLines().drop(1).toSeq.par
     }
 
-    // Process lines in parallel to create a Map[Int, String]
     val gamesRec = lines.map { line =>
-      val splitLine = line.split(",").map(_.trim) // Split by comma and trim whitespace
-      val appId = splitLine.head.toInt // Convert appId to Int
-      val title = splitLine(1) // Title is the second column
-      appId -> title // Return a tuple (appId, title)
-    }.toMap // Combine results into an immutable Map
+      val splitLine = line.split(",").map(_.trim)
+      val appId = splitLine.head.toInt
+      val title = splitLine(1)
+      appId -> title
+    }.toMap
 
     gamesRec.seq
   }
 
 def loadMetadata(path: String): Map[Int, Array[String]] = {
-  // Read the entire JSON file
+
   val source = Source.fromFile(path)
   implicit val formats: DefaultFormats.type = DefaultFormats
 
@@ -197,8 +250,8 @@ def loadMetadata(path: String): Map[Int, Array[String]] = {
     case (acc, line) =>
       val json = JsonMethods.parse(line)
       val appId = (json \ "app_id").extract[Int]
-      val tags = (json \ "tags").extract[Seq[String]].toArray  // Convert tags to Array[String]
-      acc + (appId -> tags)  // Add appId and tags to the accumulator map
+      val tags = (json \ "tags").extract[Seq[String]].toArray
+      acc + (appId -> tags)
   }
   source.close()
   appIdsAndTags
